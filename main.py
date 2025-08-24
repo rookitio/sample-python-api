752a5cf03e0932ca63063b8208cf86922dc8ba46
Sherzod Odinaev <sherzodinaev@gmail.com>
2025-08-24 17:01:25 -0400

    added POST method

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from typing import Dict
 
 app = FastAPI()
 
 @app.get("/hello")
 async def hello():
     return {"hello": "world"}
+
+@app.post("/hello")
+async def hello_post(payload: Dict):
+    return payload
